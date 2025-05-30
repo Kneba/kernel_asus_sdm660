AyushR1/AR_Beast-Zuk_Z2-4.4

v4l2-ioctl: ifndef some unhelpful warnings
This commit hides the kernel warnings when android.hardware.camera.provider@2.4-service
and ExtCamHotPlug calls v4l_querycap. Both the processes are from userspace. The warnings
really don't help us because our camera blobs are made to work with a ROM running kernel 3.18,
and those warnings have no affect in camera or video functionality running kernel 4.4. A pr_warn
is left to print 'bad caps' warning for a faulty driver and this commit can be reverted temporarily
to debug it.

Actual warnings are given below:
[   23.814648] Bad caps for driver , 200000 0
[   23.814670] ------------[ cut here ]------------
[   23.814678] WARNING: at ../drivers/media/v4l2-core/v4l2-ioctl.c:1035
[   23.814682] Modules linked in:
[   23.814688]
[   23.814696] CPU: 1 PID: 592 Comm: android.hardwar Not tainted 4.4.166 #4
[   23.814700] Hardware name: Qualcomm Technologies, Inc. MSM 8996 v3 + PMI8996 MTP (DT)
[   23.814706] task: ffffffc04556a400 task.stack: ffffffc0c7f10000
[   23.814716] PC is at v4l_querycap+0x70/0x7c
[   23.814722] LR is at v4l_querycap+0x70/0x7c
[   23.814726] pc : [<ffffff800887f9d8>] lr : [<ffffff800887f9d8>] pstate: 60400145
[   23.814730] sp : ffffffc0c7f13ac0
[   23.814734] x29: ffffffc0c7f13ad0 x28: 0000000000000000
[   23.814742] x27: ffffff8009181d38 x26: ffffffc0cbb39000
[   23.814750] x25: ffffffc04b110800 x24: ffffffc0c7f13b88
[   23.814757] x23: ffffff800917a778 x22: ffffffc04a616000
[   23.814765] x21: 0000000000000000 x20: 0000000000000000
[   23.814772] x19: ffffffc0c7f13b88 x18: 0000000000000400
[   23.814779] x17: ffffff8009958f02 x16: 0000000000000020
[   23.814787] x15: 0000000000000000 x14: 0000000000000003
[   23.814794] x13: ffffff8009958f01 x12: 00000000000003f0
[   23.814801] x11: 0000000000000000 x10: 0000000000000000
[   23.814809] x9 : cb88537fdc8ba6a6 x8 : cb88537fdc8ba6a6
[   23.814816] x7 : 69726420726f6620 x6 : ffffff8009958f2e
[   23.814823] x5 : ffffffffffffffff x4 : 0000000000000008
[   23.

drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1030,9 +1030,15 @@ static int v4l_querycap(const struct v4l2_ioctl_ops *ops,
 	 * Drivers MUST fill in device_caps, so check for this and
 	 * warn if it was forgotten.
 	 */
+#ifndef CONFIG_MACH_ZUK
 	WARN(!(cap->capabilities & V4L2_CAP_DEVICE_CAPS) ||
 		!cap->device_caps, "Bad caps for driver %s, %x %x",
 		cap->driver, cap->capabilities, cap->device_caps);
+#else
+	if (!(cap->capabilities & V4L2_CAP_DEVICE_CAPS) || !cap->device_caps)
+		pr_warn("Bad caps for driver %s, %x %x",
+		cap->driver, cap->capabilities, cap->device_caps);
+#endif
 	cap->device_caps |= V4L2_CAP_EXT_PIX_FORMAT;
 
 	return ret;

Kneba/kernel_asus_sdm660

drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1034,9 +1034,15 @@ static int v4l_querycap(const struct v4l2_ioctl_ops *ops,
 	 * Drivers MUST fill in device_caps, so check for this and
 	 * warn if it was forgotten.
 	 */
+#ifndef CONFIG_MACH_ASUS
 	WARN(!(cap->capabilities & V4L2_CAP_DEVICE_CAPS) ||
 		!cap->device_caps, "Bad caps for driver %s, %x %x",
 		cap->driver, cap->capabilities, cap->device_caps);
+#else
+	if (!(cap->capabilities & V4L2_CAP_DEVICE_CAPS) || !cap->device_caps)
+		pr_warn("Bad caps for driver %s, %x %x",
+		cap->driver, cap->capabilities, cap->device_caps);
+#endif
 	cap->device_caps |= V4L2_CAP_EXT_PIX_FORMAT;
 
 	return ret;